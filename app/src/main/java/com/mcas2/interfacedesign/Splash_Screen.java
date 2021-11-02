1f474804f5858206372c5d6547496b1788f4b782
mcas2 <mcas2@tutanota.com>
2021-11-02 09:16:59 +0100

    Splash screen 2000ms

--- app/src/main/java/com/mcas2/interfacedesign/Splash_Screen.java
+++ app/src/main/java/com/mcas2/interfacedesign/Splash_Screen.java
@@ -4,7 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class Splash_Screen extends AppCompatActivity {
 
@@ -12,12 +19,24 @@ public class Splash_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        openApp(true);
+
+        ImageView boot = findViewById(R.id.splash_boot);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        boot.startAnimation(myanim);
     }
 
-    public void openLogin (View v) {
-        Intent intent = new Intent(Splash_Screen.this, LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
+    //Esto sirve para realizar la transición después de 2000 milisegundos
+
+    public void openApp (boolean locationPermission){
+         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+             @Override
+             public void run() {
+                 Intent intent = new Intent(Splash_Screen.this, LoginActivity.class);
+                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                 startActivity(intent);
+             }
+         }, 2000);
     }
 }
