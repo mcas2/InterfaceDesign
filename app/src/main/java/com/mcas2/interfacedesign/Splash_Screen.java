e9f72e07220dee680bb5728f3dafbdc8d007fed7
mcas2 <mcas2@tutanota.com>
2021-11-02 10:05:18 +0100

    Splash Screen transition and animation

--- app/src/main/java/com/mcas2/interfacedesign/Splash_Screen.java
+++ app/src/main/java/com/mcas2/interfacedesign/Splash_Screen.java
@@ -12,6 +12,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Splash_Screen extends AppCompatActivity {
 
@@ -21,9 +22,21 @@ public class Splash_Screen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         openApp(true);
 
+        //ANIMACIÓN
         ImageView boot = findViewById(R.id.splash_boot);
-        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.movement);
         boot.startAnimation(myanim);
+
+
+
+        //FONDO
+        ImageView fondo = findViewById(R.id.backgroundSplash);
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1580436541340-36b8d0c60bae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=388&q=80")
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .centerCrop()
+                .into(fondo);
+
     }
 
     //Esto sirve para realizar la transición después de 2000 milisegundos
@@ -37,6 +50,6 @@ public class Splash_Screen extends AppCompatActivity {
                  intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                  startActivity(intent);
              }
-         }, 2000);
+         }, 3000);
     }
 }
