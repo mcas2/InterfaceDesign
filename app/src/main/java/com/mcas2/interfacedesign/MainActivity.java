d706790688ae3a9f9bc2587998afefdd6c2028aa
mcas2 <mcas2@tutanota.com>
2021-11-15 12:29:06 +0100

    Menuses

--- app/src/main/java/com/mcas2/interfacedesign/MainActivity.java
+++ app/src/main/java/com/mcas2/interfacedesign/MainActivity.java
@@ -1,8 +1,14 @@
 package com.mcas2.interfacedesign;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mainContext = (TextView) findViewById(R.id.textTap);
+        registerForContextMenu(mainContext);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.copy_context:
+                Toast toast = Toast.makeText(this, "Copiado manin", Toast.LENGTH_LONG);
+                toast.show();
+                return true;
+            case R.id.download_context:
+                Toast toast2 = Toast.makeText(this, "Descargando...", Toast.LENGTH_LONG);
+                toast2.show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
